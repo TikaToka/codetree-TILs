2cb2d096e41733a5c0867a5c8b8bf7647c96c406
Joochan Joseph Kim <49054667+TikaToka@users.noreply.github.com>
2024-10-02 19:57:53 +0900

    Title: 고대 문명 유적 탐사, Time: 5098ms, Memory: 51MB, Status: Time Limit Exceed - CodeTree

---
+++ 241002/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -0,0 +1,133 @@
+from collections import deque
+
+def rotate(graph, middle):
+    # graph는 5x5이고, middle은 3x3의 중심 좌표 (x, y)
+    x, y = middle
+
+    # 3x3 부분을 추출
+    sub_grid = [row[y-1:y+2] for row in graph[x-1:x+2]]
+
+    # 3x3 부분을 90도 시계 방향으로 회전
+    rotated = [[sub_grid[2-b][a] for b in range(3)] for a in range(3)]
+
+    # 회전된 3x3 부분을 원래 보드에 다시 삽입
+    new_board = [row[:] for row in graph]
+    for i in range(3):
+        for j in range(3):
+            new_board[x-1+i][y-1+j] = rotated[i][j]
+
+    return new_board
+
+
+def checkRange(x, y):
+    return 0<= x < 5 and 0<= y <5
+
+
+def dfs(graph):
+    dx = (-1, 0, 1, 0)
+    dy = (0, 1, 0, -1)
+    visits = []
+    visited = set()
+    for i in range(5):
+        for j in range(5):
+            tovisit = []
+            visit = set()
+            if (i, j) not in visited:
+                tovisit.append((i, j))
+                v = graph[i][j]
+                while tovisit:
+                    x, y = tovisit.pop()
+                    visit.add((x, y))
+                    for d in range(4):
+                        nx = x + dx[d]
+                        ny = y + dy[d]
+                        if checkRange(nx, ny) and (nx, ny) not in visit and (nx, ny) not in visited and graph[nx][ny]==v:
+                            tovisit.append((nx, ny))
+                visited = visited.union(visit)
+                if len(visit) >= 3:
+                    visits+=list(visit)
+    return visits
+    
+
+k, m = map(int, input().split())
+
+graph = [[] for _ in range(5)]
+for i in range(5):
+    line = [int(x) for x in input().split()]
+    graph[i] = line
+
+rep = deque([int(x) for x in input().split()])
+
+answers = []
+for i in range(k): # k
+    answer = 0
+    # center
+    maxarr = []
+    maxval = 0 # max
+    maxrot = 4 # min
+    maxq = 6 # min
+    maxp = 6 # min
+    maxgraph = [row[:] for row in graph]
+    for p in range(1, 4):
+        for q in range(1, 4):
+            tempgraph = [row[:] for row in graph]
+            # rotate
+            for m in range(1, 4):
+                tempgraph = rotate(tempgraph, (p, q))
+                arr = dfs(tempgraph)
+                if not arr:
+                    continue
+                if len(arr) > maxval: #value
+                    maxval = len(arr)
+                    maxrot = m
+                    maxarr = arr
+                    maxp = p
+                    maxq = q
+                    maxgraph = [row[:] for row in tempgraph]
+                elif len(arr) == maxval: #rotate
+                    if maxrot > m:
+                        maxrot = m
+                        maxarr = arr
+                        maxp = p
+                        maxq = q
+                        maxgraph = [row[:] for row in tempgraph]
+                    elif maxrot == m:
+                        if maxq > q:
+                            maxarr = arr
+                            maxp = p
+                            maxq = q
+                            maxgraph = [row[:] for row in tempgraph]
+                        elif maxq == q:
+                            if maxp > p:
+                                maxarr = arr
+                                maxp = p
+                                maxgraph = [row[:] for row in tempgraph]
+    if maxval == 0:
+        break
+    print(maxgraph)
+    print(maxval)
+    print(maxp, maxq)
+    print(maxrot)
+    answer += maxval
+    if not maxarr:
+        continue
+    while True:
+        # refill
+        maxarr = sorted(maxarr, key=lambda x: (x[1], -x[0]))
+        print(maxarr)
+        for x, y in maxarr:
+            if rep:
+                maxgraph[x][y] = rep.popleft()
+        print(maxgraph)
+        # check
+        arr = dfs(maxgraph) # 여기가 이상하게 나옴
+        if not arr:
+            break
+        else:
+            maxarr = arr
+            answer += len(arr)
+    if answer != 0:
+        answers.append(str(answer))
+
+if answer:
+    print(' '.join(answers))
