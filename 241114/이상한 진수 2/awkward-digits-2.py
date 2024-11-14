c75fa4145a6fe1dad87c2200dada16399bb0d64e
Joochan Joseph Kim <49054667+TikaToka@users.noreply.github.com>
2024-11-14 19:40:54 +0900

    Title: 이상한 진수 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/이상한 진수 2/awkward-digits-2.py
+++ 241114/이상한 진수 2/awkward-digits-2.py
@@ -7,9 +7,19 @@ def binary2decimal(n):
         cnt += 1
     return output
 n = list(input())
+
+changed = False
 for i in range(len(n)):
     if n[i] == '0':
         n[i] = '1'
+        changed = True
         break
+
+if not changed:
+    if n[-1] == '1':
+        n[-1] = '0'
+    elif n[-1] == '0':
+        n[-1] = '1'
+
 answer = binary2decimal(n)
 print(answer)
