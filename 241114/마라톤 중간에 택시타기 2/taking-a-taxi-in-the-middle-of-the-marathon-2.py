92f5f3a602e4330387435996fb84048a09e5baae
Joochan Joseph Kim <49054667+TikaToka@users.noreply.github.com>
2024-11-14 22:59:41 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 60ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241114/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,22 @@
+def mDist(n1, n2):
+    x1, y1 = n1
+    x2, y2 = n2
+    return abs(x1-x2) + abs(y1-y2)
+
+N = int(input())
+ckpt = []
+for i in range(N):
+    x, y = map(int, input().split())
+    ckpt.append((x, y))
+
+answer = 10000
+
+for i in range(1, N-1):
+    temp = ckpt[:]
+    temp.pop(i)
+    cand = 0
+    for j in range(len(temp)-1):
+        cand += mDist(temp[j], temp[j+1])
+    answer = min(answer, cand)
+
+print(answer)
