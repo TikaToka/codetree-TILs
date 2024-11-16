54459dc6c0e9307cc3fa09663edf43ed92844c67
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-11-16 21:21:39 +0900

    Title: 정수 사각형 최소 합, Time: 63ms, Memory: 17MB, Status: Passed - CodeTree

--- 241116/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ 241116/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -6,8 +6,8 @@ dp = [[99999999999999999999999 for _ in range(n)] for _ in range(n)]
 
 dp[0][n-1] = board[0][n-1]
 
-for i in range(0, n):
-    dp[i][0] = dp[i-1][0] + board[i][0]
+for i in range(1, n):
+    dp[i][n-1] = dp[i-1][n-1] + board[i][n-1]
 
 for i in range(n-2, -1, -1):
     dp[0][i] = dp[0][i+1] + board[0][i]
