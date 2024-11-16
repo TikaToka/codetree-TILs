7401dfa093d7536e3313cf175d4aea295989dfa5
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-11-16 22:08:16 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241116/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 241116/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -9,11 +9,11 @@ for i in range(1, n):
     dp[i][0] = min(board[i][0], dp[i-1][0])
     dp[0][i] = min(board[0][i], dp[0][i-1])
 
-for i in range(1, n-1):
-    for j in range(1, n-1):
+for i in range(1, n):
+    for j in range(1, n):
         dp[i][j] = min(dp[i-1][j], dp[i][j-1], board[i][j])
 
-if dp[n-1][n-1] >= board[n-1][n-1]:
-    print(board[n-1][n-1])
-elif dp[n-1][n-1] < board[n-1][n-1]:
-    print(max(dp[n-2][n-1], dp[n-1][n-2]))
+if dp[n-1][n-1] < board[n-1][n-1]:
+    dp[n-1][n-1] = max(dp[n-2][n-1], dp[n-1][n-2])
+
+print(dp)
