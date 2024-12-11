87744f10747a19ffe6464ba78f9dfd1f97134e2d
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-11 22:03:44 +0900

    Title: 최대 증가 부분 수열, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241211/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,15 @@
+n = int(input())
+board = [x for x in map(int, input().split())]
+
+dp = [-1 for _ in range(n)]
+dp[0] = 0
+
+for i in range(1, n):
+    for j in range(0, i):
+        if dp[j] == -1:
+            continue
+        if board[j] + j >= i:
+            dp[i] = max(dp[i], dp[j]+1)
+
+
+print(max(dp))
