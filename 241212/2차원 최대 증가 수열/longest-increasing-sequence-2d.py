e71a357432695349c1e70a192adc3eefd4a2fbb9
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-12 17:26:31 +0900

    Title: 2차원 최대 증가 수열, Time: 56ms, Memory: 17MB, Status: Passed - CodeTree

--- 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -2,13 +2,15 @@ n, m = map(int, input().split())
 
 board = [[x for x in map(int, input().split())] for _ in range(n)]
 
-dp = [[1 for _ in range(m)] for _ in range(n)]
+dp = [[-1 for _ in range(m)] for _ in range(n)]
+dp[0][0] =1
 
-
-for i in range(0, n):
-    for j in range(0, m):
+for i in range(1, n):
+    for j in range(1, m):
         for a in range(0, i):
             for b in range(0, j):
+                if dp[a][b] == -1:
+                    continue
                 if board[i][j] > board[a][b]:
                     dp[i][j] = max(dp[i][j], dp[a][b] + 1)
 
