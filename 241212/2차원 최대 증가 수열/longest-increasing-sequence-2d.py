9dd4758e2dd4c957b050056ced70a0de0320972b
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-12 15:29:11 +0900

    Title: 2차원 최대 증가 수열, Time: 80ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -8,7 +8,7 @@ dp = [[1 for _ in range(m)] for _ in range(n)]
 for i in range(1, n):
     for j in range(1, m):
         for a in range(0, i):
-            for b in range(0, i):
+            for b in range(0, j):
                 if board[i][j] > board[a][b]:
                     dp[i][j] = max(dp[i][j], dp[a][b] + 1)
 
