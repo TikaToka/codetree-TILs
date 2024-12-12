e3de614606acaea70f04c76d27cb06fd481f7ecc
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-12 15:27:56 +0900

    Title: 2차원 최대 증가 수열, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+board = [[x for x in map(int, input().split())] for _ in range(n)]
+
+dp = [[1 for _ in range(m)] for _ in range(n)]
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        for a in range(0, i):
+            for b in range(0, i):
+                if board[i][j] > board[a][b]:
+                    dp[i][j] = max(dp[i][j], dp[a][b] + 1)
+
+answer = 0
+for i in range(n):
+    for j in range(m):
+        answer = max(answer, dp[i][j])
+
+print(answer)
