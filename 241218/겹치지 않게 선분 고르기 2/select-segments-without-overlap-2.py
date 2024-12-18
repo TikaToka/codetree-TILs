d78e1615bc6faa958d41720e228fd15b15086f8a
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-18 20:37:56 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 77ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241218/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+dp = [1 for _ in range(n)]
+
+
+board = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    board.append((a, b))
+
+
+board.sort(key=lambda x: x[1])
+
+
+for i in range(1, n):
+    for j in range(0, i):
+        if board[j][1] <= board[i][0]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
+
