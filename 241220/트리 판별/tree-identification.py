c2d6c183a07604ebc19d49cfa2dfb06d9d585925
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-20 17:03:50 +0900

    Title: 트리 판별, Time: 52ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241220/트리 판별/tree-identification.py
+++ 241220/트리 판별/tree-identification.py
@@ -8,39 +8,41 @@ for i in range(m):
     edges[y].append(x)
 
 
-parent = [0] * (10001)
-visited = [False] * (10001)
+# parent = [0] * (10001)
+# visited = [False] * (10001)
 
-def dfs(x):
-    visited[x] = True
-    for y in edges[x]:
-        if not visited[y]:
-            parent[y] = x
-            dfs(y)
+# def dfs(x):
+#     visited[x] = True
+#     for y in edges[x]:
+#         if not visited[y]:
+#             parent[y] = x
+#             dfs(y)
 
-dfs(1)
+# dfs(1)
 
-edges = [[] for _ in range(10001)]
+# edges = [[] for _ in range(10001)]
 
-for i in range(1, len(parent)):
-    if parent[i] != 0:
-        edges[parent[i]].append(i)
+# for i in range(1, len(parent)):
+#     if parent[i] != 0:
+#         edges[parent[i]].append(i)
 
 visited = [False] * (10001)
 
 cnt = 0
 def dfs(x):
     global cnt
-    tovisit = [x]
+    tovisit = [(x, 0)]
     while tovisit:
         cnt += 1
-        x = tovisit.pop()
+        x, p = tovisit.pop()
         visited[x] = True
         for y in edges[x]:
+            if y == p:
+                continue
             if visited[y]:
                 return 0
             else:
-                tovisit.append(y)
+                tovisit.append((y, x))
     return 1
 
 
