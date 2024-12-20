2f108e2cb30021448905782a337fafc5eaa95457
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-20 15:05:28 +0900

    Title: 트리의 지름, Time: 5028ms, Memory: 39MB, Status: Time Limit Exceed - CodeTree

---
+++ 241220/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,51 @@
+n = int(input())
+
+edges = [[] for _ in range(n+1)]
+
+visited = [False] * (n+1)
+
+parent = [()] * (n+1)
+
+for i in range(n-1):
+    x, y, w = map(int, input().split())
+    edges[x].append((y, w))
+    edges[y].append((x, w))
+
+
+def traversal(x):
+    for y, w in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            parent[y] = (x, w)
+
+            traversal(y)
+
+traversal(1)
+
+def dfs(start):
+    maxval = 0
+    maxidx = start
+    visited = []
+    tovisit = [(start, 0)]
+    while tovisit:
+        (node, w) = tovisit.pop()
+        visited.append(node)
+        if w > maxval:
+            maxval = w
+            maxidx = node
+        if parent[node] != ():
+            i = parent[node]
+            if i[0] not in visited and i[0] not in tovisit:
+                tovisit.append((i[0], w+i[1]))
+        for i in edges[node]:
+            if i[0] not in visited and i[0] not in tovisit:
+                    tovisit.append((i[0], w+i[1]))
+
+    return maxidx, maxval
+
+idx, val = dfs(1)
+
+idx, val = dfs(idx)
+
+print(val)
+    
