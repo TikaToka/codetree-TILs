fa9767ee2e8693848cf410d42de9a770c9c46340
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-20 17:07:41 +0900

    Title: 트리 판별, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241220/트리 판별/tree-identification.py
+++ 241220/트리 판별/tree-identification.py
@@ -5,26 +5,7 @@ edges = [[] for _ in range(10001)]
 for i in range(m):
     x, y = map(int, input().split())
     edges[x].append(y)
-    edges[y].append(x)
 
-
-# parent = [0] * (10001)
-# visited = [False] * (10001)
-
-# def dfs(x):
-#     visited[x] = True
-#     for y in edges[x]:
-#         if not visited[y]:
-#             parent[y] = x
-#             dfs(y)
-
-# dfs(1)
-
-# edges = [[] for _ in range(10001)]
-
-# for i in range(1, len(parent)):
-#     if parent[i] != 0:
-#         edges[parent[i]].append(i)
 
 visited = [False] * (10001)
 
