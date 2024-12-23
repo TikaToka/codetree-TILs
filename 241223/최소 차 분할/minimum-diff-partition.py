e7fb6b78d9b29967e10967d4a86ca172b939fb45
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-23 23:40:43 +0900

    Title: 최소 차 분할, Time: 52ms, Memory: 17MB, Status: Passed - CodeTree

--- 241223/최소 차 분할/minimum-diff-partition.py
+++ 241223/최소 차 분할/minimum-diff-partition.py
@@ -6,16 +6,14 @@ nums = list(map(int, input().split()))
 
 total = sum(nums)
 
-dp = [0] * (n+1)
-dp2 = [0] * (n+1)
+dp = [False] * (total+1)
+dp[total] = True
+dp[0] = True
 for i in range(len(nums)):
-    for j in range(n-1, 0, -1):
-        dp[j] = max(dp[j], dp[j-1] + nums[i]) # abs((dp[j] - nums[i]) - (dp[j-1] + nums[i])))
-        dp2[j] = total - dp[j]
+    for j in range(total, 0, -1):
+        dp[j] = dp[j] or dp[j-nums[i]]
 
-answer = sys.maxsize
-
-for i in range(1, n):
-    answer = min(abs(dp[i] - dp2[i]), answer)
-
-print(answer)
+for i in range(len(dp)//2, 0, -1):
+    if dp[i]:
+        print(abs(total-2*i))
+        break
