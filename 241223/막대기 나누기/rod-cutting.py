9d5a5cf4b99b0d3f33e46de6a43982c627917126
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-23 19:32:15 +0900

    Title: 막대기 나누기, Time: 50ms, Memory: 17MB, Status: Passed - CodeTree

--- 241223/막대기 나누기/rod-cutting.py
+++ 241223/막대기 나누기/rod-cutting.py
@@ -4,8 +4,8 @@ bars += list(map(int, input().split()))
 dp = [0] * (n+1)
 
 
-for i in range(n+1):
-    for j in range(1, 5):
+for i in range(1, n+1):
+    for j in range(1, n+1):
         if i>=j:
             dp[i] = max(dp[i-j] + bars[j], dp[i])
 print(dp[n])
