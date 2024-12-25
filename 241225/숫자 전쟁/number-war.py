72582e7f2a681b256300ff2402dbf1a3bd763531
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2024-12-25 23:50:01 +0900

    Title: 숫자 전쟁, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241225/숫자 전쟁/number-war.py
+++ 241225/숫자 전쟁/number-war.py
@@ -1,25 +1,32 @@
+# 각 플레이어의 카드 정보를 입력받습니다.
 n = int(input())
+a = [0] + list(map(int, input().split()))
+b = [0] + list(map(int, input().split()))
 
-a = [0]
-a += list(map(int, input().split()))
-b = [0]
-b += list(map(int, input().split()))
+# dp 배열을 초기화합니다. 초기값은 -1로 설정합니다.
+dp = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
 
-dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+# 기본 케이스를 설정합니다.
+dp[0][0] = 0
 
+# 각 경우의 수를 동적 프로그래밍으로 계산합니다.
+# dp[i][j] :: 첫 번째 플레이어는 i번 카드까지, 두 번째 플레이어는 j번 카드까지 버렸을 때 나올 수 있는 최대 점수
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == -1:
+            continue
 
+        # 카드 대결 - 첫 번째 플레이어의 카드가 더 작은 경우
+        if a[i + 1] < b[j + 1]:
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if a[i] <= b[j]:
-            # if dp[i-1][j-1] != 0:
-            dp[i][j] = max(dp[i-1][j-1], dp[i][j])
-            # if dp[i-1][j] != 0:
-            dp[i][j] = max(dp[i-1][j], dp[i][j])
-        else:
-            # if dp[i][j-1] != 0:
-            dp[i][j] = max(dp[i][j], dp[i][j-1] + b[j])
-        
+        # 카드 대결 - 두 번째 플레이어의 카드가 더 작은 경우
+        if a[i + 1] > b[j + 1]:
+            dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + b[j + 1])
+
+        # 카드 버리기
+        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j])
+
 # 결과를 계산하여 출력합니다.
 ans = 0
 for i in range(n + 1):
