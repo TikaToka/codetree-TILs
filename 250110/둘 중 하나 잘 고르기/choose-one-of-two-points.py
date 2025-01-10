46c6c4bb138eda5b7ee193e0e4fe7953e75ced64
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2025-01-10 23:41:05 +0900

    Title: 둘 중 하나 잘 고르기, Time: 65ms, Memory: 17MB, Status: Passed - CodeTree

--- 250110/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 250110/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -20,10 +20,6 @@ for i in range(1, 2*n):
         else:
             dp[i][j] = max(dp[i][j], dp[i-1][j-1] + red[i], dp[i-1][j] + blue[i])
 
-answer = 0
-for i in range(2*n):
-    for j in range(n+1):
-        if answer < dp[i][j]:
-            answer = dp[i][j]
 
-print(answer)
+
+print(dp[2*n-1][n])
