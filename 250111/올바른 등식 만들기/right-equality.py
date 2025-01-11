ce73b8025efaed4e56e3b274cef55935354e6a66
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2025-01-11 21:43:53 +0900

    Title: 올바른 등식 만들기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/올바른 등식 만들기/right-equality.py
@@ -0,0 +1,27 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+# Write your code here!
+dp = [[-99 for _ in range(2**(N-1))] for _ in range(N)]
+dp[0][0] = nums[0]
+
+for i in range(1, N):
+    for j in range(2**(N-1)):
+        if dp[i-1][j//2] != -99:
+            if j % 2 == 0:
+                temp = dp[i-1][j//2] + nums[i]
+                if temp <= 20:
+                    dp[i][j] = temp
+            else:
+                temp = dp[i-1][j//2] - nums[i]
+                if temp >= -20:
+                    dp[i][j] = temp
+
+print(dp)
+
+answer = 0
+for i in range(2**(N-1)):
+    if dp[N-1][i] == M:
+        answer += 1
+
+print(answer)
