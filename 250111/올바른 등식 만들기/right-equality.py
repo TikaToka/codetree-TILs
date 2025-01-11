d086f5a0a5c826f1d4d47e83764d21225c42ae67
Joochan Kim <49054667+TikaToka@users.noreply.github.com>
2025-01-11 22:40:38 +0900

    Title: 올바른 등식 만들기, Time: 50ms, Memory: 17MB, Status: Passed - CodeTree

--- 250111/올바른 등식 만들기/right-equality.py
+++ 250111/올바른 등식 만들기/right-equality.py
@@ -1,25 +1,24 @@
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
 
-# Write your code here!
-cand = []
-cand.append(nums[0])
-cand.append(-1 * nums[0])
+OFFSET = 20
+dp = [0] * (2 * OFFSET + 1)  # 가능한 값 범위 [-20, 20]을 저장
+
+# 첫 번째 숫자를 초기화
+dp[nums[0] + OFFSET] += 1
+dp[-nums[0] + OFFSET] += 1
+
+# 각 단계별로 가능한 상태만 업데이트
 for i in range(1, N):
-    temp = []
-    for j in cand:
-        c = j + nums[i]
-        if c <= 20:
-            temp.append(j + nums[i])
-        c = j - nums[i]
-        if c >= -1 * 20:
-            temp.append(j - nums[i])
-    cand = temp
+    next_dp = [0] * (2 * OFFSET + 1)  # 현재 단계의 유효한 상태만 추적
+    for x in range(-OFFSET, OFFSET + 1):
+        if dp[x + OFFSET] > 0:  # 이전 단계에서 유효했던 상태만 사용
+            # 숫자를 더하거나 뺀 경우
+            if x + nums[i] <= OFFSET:
+                next_dp[x + nums[i] + OFFSET] += dp[x + OFFSET]
+            if x - nums[i] >= -OFFSET:
+                next_dp[x - nums[i] + OFFSET] += dp[x + OFFSET]
+    dp = next_dp  # 현재 단계의 상태를 갱신
 
-answer = 0
-
-for i in cand:
-    if i == M:
-        answer += 1
-
-print(answer)
+# 최종 결과 출력
+print(dp[M + OFFSET])
